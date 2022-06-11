7d7b30728787be70b95f0d7af40f994b9ccfb3b9
Ryan Barrera <rbarrera9er@gmail.com>
2022-06-11 13:06:52 -0400

    commit tree fd1dc64b7a04d97a66d2a8b43ff97e80ff3acc7f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainElement = document.querySelector("#main");
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Ryan is the champion";
